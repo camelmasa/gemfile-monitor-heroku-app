1519d444dbf76a11253080f4a5ac6fb62967e61f
camelmasa <camelmasa@gmail.com>
2013-06-01 17:41:08 +0900

    first commit

---
+++ gem-monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+SHELL=/bin/bash
+
+# account
+username=""
+password=""
+repository=""
+
+# repository
+url="github.com"
+date=`date '+%Y-%m-%d'`
+branch="update-gemfile-${date}"
+
+mv bundle_config .bundle/config
+git clone https://${username}:${password}@${url}/${repository}.git repo
+cd repo
+git checkout -b ${branch}
+bundle update
+git add Gemfile.lock
+git commit -m 'update Gemfile'
+git push origin ${branch}
+hub pull-request  ${branch}
